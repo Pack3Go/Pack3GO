8c436bd30085e423d8ec5fe46348c0686ad8ae59
pack3go <32922710+Pack3Go@users.noreply.github.com>
2017-10-19 18:54:09 -0500

    1
    1

--- js/seguimiento.js
+++ js/seguimiento.js
@@ -1,41 +1,46 @@
-function loadXMLDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-	if (this.readyState == 4 && this.status == 200) {
-	  var jsoninfo = this.responseText;
-	  var myObjInfo = JSON.parse(jsoninfo.toString());
-	  var sinInfoGuia = "No se cuenta con información de esta guia.";
-	  var vacioGuia = "Favor de capturar una guia valida.";
-	  var headCabecera = 
-	  "<table border='2'><tr><td>Fecha</td><td>Hora</td><td>Movimiento</td><td>Comentario</td></tr>";
-	  var cadenaString = "";
-	  if (document.getElementById("text_guia").value !== "") {
-		  for (i = 0; i < myObjInfo.length; i++) {
-			if (myObjInfo[i].idseguimiento === document.getElementById("text_guia").value) {
-				for (index = 0; index < myObjInfo[i].fecha.length; index++) {
-					var fec = myObjInfo[i].fecha[index] == undefined ? "":myObjInfo[i].fecha[index];
-					var hrs = myObjInfo[i].hora[index] == undefined ? "":myObjInfo[i].hora[index];
-					var mov = myObjInfo[i].movimiento[index] == undefined ? "":myObjInfo[i].movimiento[index];
-					var comen = myObjInfo[i].comentarios[index] == undefined ? "":myObjInfo[i].comentarios[index];
-					cadenaString += "<tr><td>" + fec + "</td>"
-								+"<td>" + hrs + "</td>"
-								+"<td>" + mov + "</td>"
-								+"<td>" + comen + "</td></tr>";
-				}
-				var ponInfo = headCabecera+cadenaString+"</table>";
-				document.getElementById("divDetalleGuia").innerHTML = ponInfo;
-				document.getElementById("msg").innerHTML = "";
-			} else {
-				document.getElementById("divDetalleGuia").innerHTML = "";
-				document.getElementById("msg").innerHTML = sinInfoGuia;
-			}
-		  }
-	  } else {
-		document.getElementById("divDetalleGuia").innerHTML = "";
-		document.getElementById("msg").innerHTML = vacioGuia;
-	  }
-	}
-  };
-  xhttp.open("GET", "JS/pack3go.json", true);
-  xhttp.send();
-}
+$(document).ready(function(){
+$("#danger").hide();
+$("#divDetalleGuia").hide();
+var myObjInfo = $.parseJSON('[{"idseguimiento": "EE987271825CN","fecha":["18-sep-2017","18-sep-2017","18-sep-2017","18-sep-2017","19-sep-2017","19-sep-2017","19-sep-2017","06-oct-2017","06-oct-2017","06-oct-2017","06-oct-2017","06-oct-2017"],"hora":["11:45","13:34","19:30","19:45","11:40","17:20","17:20","07:20","13:40","19:40","08:15","13:45"],"movimiento":["Aprobado por el correo Ciudad de México","Recolectado Ciudad de México","Llegada a CEDIS","Movimiento a CEDIS","Salida de CEDIS Ciudad de México","Regreso a CEDIS Ciudad de México","Salida de CEDIS Ciudad de México","Procesado para salida Ciudad de México","Salida a estación de transito Ciudad de México","Llagada a estación de transito Ciudad de México","Procesado para salida de estación"], "comentarios":["","se traslada"]}]');
+	
+	$("#btn_consultas").click(function(){
+		 alert("hola " + myObjInfo.toString());
+				  //var myObjInfo = $.parseJSON(jsoninfo.toString());
+				  var sinInfoGuia = "No se cuenta con información de esta guia.";
+				  var vacioGuia = "Favor de capturar una guia valida.";
+				  var headCabecera = 
+				  "<table class='table'><thead class='thead-dark'><tr><th scope='col'>Fecha</th><th scope='col'>Hora</th><th scope='col'>Movimiento</th><th scope='col'>Comentario</th></tr></thead><tbody>";
+				  var cadenaString = "";
+				  if ($("#text_guia").val() !== "") {
+					  for (i = 0; i < myObjInfo.length; i++) {
+						if (myObjInfo[i].idseguimiento === $("#text_guia").val()) {
+							for (index = 0; index < myObjInfo[i].fecha.length; index++) {
+								var fec = myObjInfo[i].fecha[index] == undefined ? "":myObjInfo[i].fecha[index];
+								var hrs = myObjInfo[i].hora[index] == undefined ? "":myObjInfo[i].hora[index];
+								var mov = myObjInfo[i].movimiento[index] == undefined ? "":myObjInfo[i].movimiento[index];
+								var comen = myObjInfo[i].comentarios[index] == undefined ? "":myObjInfo[i].comentarios[index];
+								cadenaString += "<tr><td>" + fec + "</td>"
+											+"<td>" + hrs + "</td>"
+											+"<td>" + mov + "</td>"	
+											+"<td>" + comen + "</td></tr>";
+							}
+							var ponInfo = headCabecera+cadenaString+"<tbody></table>";
+							$("#divDetalleGuia").attr(ponInfo);
+							$("#divDetalleGuia").show();
+							$("#danger").attr("");
+							$("#danger").hide();
+						} else {
+							$("#divDetalleGuia").attr("");
+							$("#divDetalleGuia").hide();
+							$("#danger").attr(sinInfoGuia);
+							$("#danger").show();
+						}
+					  }
+				  } else {
+					$("#divDetalleGuia").attr("");
+					$("#divDetalleGuia").hide();
+					$("#danger").attr(vacioGuia);
+					$("#danger").show();
+				  }
+	});
+});
